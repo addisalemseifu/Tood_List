1be1000351f0724587ba7516e22342debe792e89
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-04-06 09:18:02 +0100

    Add the test for edit task in UI

--- src/edit&update.test.js
+++ src/edit&update.test.js
@@ -44,3 +44,59 @@ describe('Eddit task changes in local storage', () => {
       { task: 'changed Task', completed: false, index: 2 }]);
   });
 });
+
+describe("Eddit task changes in UI", () => {
+  it("should add and remove classes from elements", () => {
+    const myTask = {
+      task: "",
+      completed: false,
+      index: 0,
+    };
+    const newBook = document.getElementsByClassName("newBook");
+    const tasks = document.getElementsByClassName("newBook");
+
+    myTask.task = "Task1";
+    myTask.index = 1;
+    UI.addTaskToList(myTask);
+    expect(newBook.length).toBe(1);
+    myTask.task = "Task2";
+    myTask.index = 2;
+    UI.addTaskToList(myTask);
+    expect(newBook.length).toBe(2);
+    myTask.task = "Task3";
+    myTask.index = 3;
+    UI.addTaskToList(myTask);
+    UI.editable(tasks[0]);
+    UI.editable(tasks[0]);
+
+    expect(tasks[0].classList.contains("book_active")).toEqual(true);
+    expect(
+      tasks[0].lastElementChild.lastElementChild.classList.contains(
+        "trash_active"
+      )
+    ).toEqual(true);
+    expect(
+      tasks[0].lastElementChild.firstElementChild.classList.contains(
+        "drop_deactive"
+      )
+    ).toEqual(true);
+  });
+
+  it("should remove completed tasks from UI", () => {
+    const newBook = document.getElementsByClassName("newBook");
+    const checked = document.getElementsByClassName("check");
+    checked[1].checked = 1;
+
+    UI.clearDone();
+
+    expect(newBook.length).toEqual(2);
+  });
+
+  it("should clean the task input bar", () => {
+    const inputFild = document.querySelector("#txtTitle");
+
+    UI.clearFields();
+
+    expect(inputFild.value).toEqual("");
+  });
+});
